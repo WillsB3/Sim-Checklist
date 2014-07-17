34db69194711cf145a9b88d3a248444f18cedfa1
Wills Bithrey <wills@potatolondon.com>
2014-07-17 18:31:15 +0100

    WIP Model things

--- simchecklist/core/admin.py
+++ simchecklist/core/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 from . import models
 
 # Register models which use default admin classes.
-# admin.site.register([])
+admin.site.register([models.Aircraft, models.Checklist, models.ChecklistPhase, models.ChecklistStep, models.User])
--- simchecklist/core/models.py
+++ simchecklist/core/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
+class Aircraft(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Aircraft'
+
+    def __unicode__(self):
+        return self.name
+
+class Checklist(models.Model):
+    aircraft = models.OneToOneField(Aircraft)
+
+class ChecklistPhase(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+class ChecklistStep(models.Model):
+    item = models.CharField(max_length=200)
+    action = models.CharField(max_length=200)
+    # steps = models.OneToManyField(ChecklistStep)
+
 class User(AbstractUser):
+    aircraft = models.ManyToManyField(Aircraft)
     objects = UserManager()
