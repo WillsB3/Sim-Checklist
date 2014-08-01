c37a65bd8377605b6628cc21998a3fce62d163bb
Wills Bithrey <wills@willsbithrey.com>
2014-08-01 07:48:34 +0100

    Make API read only by everyone

--- simchecklist/settings.py
+++ simchecklist/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
     'PAGINATE_BY': 10
 }
 
