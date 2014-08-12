071dfacc8db9334025e496f4dcdd13e69b87f15c
Ben Mason <ben@sharkbyte.co.uk>
2014-08-12 22:14:06 +0100

    Correct the location of the static files and some housekeeping.

--- simchecklist/settings/production.py
+++ simchecklist/settings/production.py
@@ -11,14 +11,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static/build/'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 SOUTH_DATABASE_ADAPTERS = {
     'default': 'south.db.postgresql_psycopg2'
